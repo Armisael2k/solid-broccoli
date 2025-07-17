4e7e3f7635a92a7c69576e11bd180c9c723b8bca
Armisael2k <chuylopez755@gmail.com>
2025-07-16 18:51:32 -0700

    fix

---
+++ brocco_1.lua
@@ -0,0 +1,6 @@
+config = {
+  type = "1v1",
+  name = "Brocco_1",
+  team1 = {"m0tar_vKkpXFSp"},
+  team2 = {"m0tar_EQmmO5G9laX2R"}
+};
---
+++ protein.lua
@@ -0,0 +1,4 @@
+local url = "https://raw.githubusercontent.com/Armisael2k/solid-broccoli/refs/heads/main/brocco_1.json";
+loadstring(game:HttpGet(url))();
+
+print(config)
